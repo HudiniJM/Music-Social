cb40fc2c0461b1e9ed4a07011dc53b7d502db1bb
HudiniJM <djmitchellradio@gmail.com>
2016-12-23 11:38:32 +0400

    feed part1 done

---
+++ Music-Social/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  Music-Social
+//
+//  Created by Jonathan Mitchell on 12/22/16.
+//  Copyright © 2016 JDM FZJ. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
