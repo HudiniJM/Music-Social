842e9cc21f4ba9292c50b5dadb082e6ce3a2eb49
HudiniJM <djmitchellradio@gmail.com>
2016-12-25 00:29:38 +0400

    firebase get completed

---
+++ Music-Social/Post.swift
@@ -0,0 +1,55 @@
+//
+//  Post.swift
+//  Music-Social
+//
+//  Created by Jonathan Mitchell on 12/24/16.
+//  Copyright © 2016 JDM FZJ. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postkey: String!
+    
+    var caption: String {
+        return _caption
+    }
+    
+    var imageUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postKey: String {
+        return _postkey
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._imageUrl = imageUrl
+        self._likes = likes
+    }
+    
+    init(postKey: String, postData: Dictionary<String, Any>) {
+        self._postkey = postKey
+        
+        if let caption = postData["caption"] as? String{
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+    }
+}
